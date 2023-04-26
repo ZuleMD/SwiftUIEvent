6d207a194fe7332827909085dfc90df0e48983b6
orangeodc2 <orangeodc2@iMac-de-orange.local>
2023-04-26 16:27:23 +0100

    Feat 3 : getAllDeatils + UIDetailView + getAllUpcomingEvents

---
+++ Eventapp/Model/EventDetail.swift
@@ -0,0 +1,8 @@
+//
+//  EventDetail.swift
+//  Eventapp
+//
+//  Created by orangeodc2 on 26/4/2023.
+//
+
+import Foundation
---
+++ Eventapp/View/DetailsEventView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailsEventView.swift
+//  Eventapp
+//
+//  Created by orangeodc2 on 26/4/2023.
+//
+
+import SwiftUI
+
+struct DetailsEventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailsEventView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailsEventView()
+    }
+}
